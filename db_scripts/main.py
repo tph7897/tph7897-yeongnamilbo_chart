a09e9c5a4f44af38817033ea232f456e14732ed4
miobix <c.aravena90@gmail.com>
2025-02-26 15:10:03 +0900

    db task init

---
+++ db_scripts/main.py
@@ -0,0 +1,7 @@
+
+if __name__ == "__main__":
+        print("Init main file")
+        # get start end dates
+        # articles_data_snapshot = yndb.get_all_articles_for_period(start_date, end_date)
+        # result = mongo_db.handle_article_batch(article_data_snapshot)
+        # print result

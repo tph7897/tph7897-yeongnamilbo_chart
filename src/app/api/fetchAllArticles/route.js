f5530af297372da7c453dbe580dd30d9ab1b91b0
tph7897 <132332533+tph7897@users.noreply.github.com>
2025-04-28 17:41:51 +0900

    fix: update articles query to only fetch data from the last three months

--- src/app/api/fetchAllArticles/route.js
+++ src/app/api/fetchAllArticles/route.js
@@ -8,21 +8,24 @@ export async function GET(request) {
     const database = client.db("yeongnam-visits");
     const articlesCollection = database.collection("full_data");
 
-    // Specify exactly the fields you want to retrieve
+    // 최근 3개월 전 날짜 계산
+    const threeMonthsAgo = new Date();
+    threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+
+    // 조회할 필드 지정
     const projection = {
-      newsdate : 1, 
-      newskey : 1,
-      code_name : 1,
-      byline_gijaname: 1, 
+      newsdate: 1,
+      newskey: 1,
+      code_name: 1,
+      byline_gijaname: 1,
       newsclassid: 1,
       newsclass_names: 1,
-      newstitle: 1, 
+      newstitle: 1,
       ref: 1,
-
     };
 
     const optimizedArticlesData = await articlesCollection
-      .find({ ref: { $exists: true } })
+      .find({ ref: { $exists: true }, newsdate: { $gte: threeMonthsAgo } })
       .project(projection)
       .sort({ newsdate: -1 })
       .toArray();
@@ -34,9 +37,9 @@ export async function GET(request) {
     }
   } catch (error) {
     console.error("Data fetch error:", error);
-    return Response.json({ 
-      message: "Nextjs Route Error", 
-      error: error.message 
+    return Response.json({
+      message: "Nextjs Route Error",
+      error: error.message,
     }, { status: 500 });
   } finally {
     await client.close();
